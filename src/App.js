c6bb2a5f939daef942c8ab53f4ef6b761baf5d03
Rostyslav <rklyba@yahoo.com>
2020-07-03 08:58:56 +0300

    This is my first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import Header from "./components/ui/Header";
+import "./App.css";
+
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  return <div className="container">
+    <Header />
+  </div>;
+};
+
+export default App;
